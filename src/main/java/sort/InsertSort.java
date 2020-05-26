7d2196ea34663b40d6e8897e417aa5e1bd23cd96
mei <abbymei1994@163.com>
2020-05-26 20:44:43 +0800

    InsertSort

--- src/main/java/sort/InsertSort.java
+++ src/main/java/sort/InsertSort.java
@@ -3,7 +3,14 @@ package sort;
 public class InsertSort {
     public static void insertSort(int[] array){
         for(int i = 1; i < array.length; i++){
-
+            for(int j = i; j > 0; j--){
+                int temp = array[j];
+                while (temp < array[j-1]){
+                    array[j] = array[j-1];
+                    j--;
+                }
+                array[j] = temp;
+            }
         }
     }
 }
