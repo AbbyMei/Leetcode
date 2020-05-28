a7e42b4ab45cd0a09a0209900d039c2b6f017141
meilignyun <meilingyun.meituan.com>
2020-05-28 23:29:53 +0800

    half of mergeSort

--- src/main/java/sort/MergeSort.java
+++ src/main/java/sort/MergeSort.java
@@ -11,49 +11,26 @@ import java.util.Arrays;
  * @Modified By:
  */
 public class MergeSort {
-    public static void merge(int[] a, int low, int mid, int high) {
-        int[] temp = new int[high - low + 1];
-        int i = low;// 左指针
-        int j = mid + 1;// 右指针
-        int k = 0;
-        // 把较小的数先移到新数组中
-        while (i <= mid && j <= high) {
-            if (a[i] < a[j]) {
-                temp[k++] = a[i++];
-            } else {
-                temp[k++] = a[j++];
-            }
+    
+    public static void mergeSort(int[] array, int startIndex, int endIndex){
+        int mid = (startIndex + endIndex)/2;
+        if(startIndex == endIndex){
+            return;
         }
-        // 把左边剩余的数移入数组
-        while (i <= mid) {
-            temp[k++] = a[i++];
-        }
-        // 把右边边剩余的数移入数组
-        while (j <= high) {
-            temp[k++] = a[j++];
-        }
-        // 把新数组中的数覆盖nums数组
-        for (int k2 = 0; k2 < temp.length; k2++) {
-            a[k2 + low] = temp[k2];
-        }
+        mergeSort(array,startIndex,mid);
+        mergeSort(array,mid+1,endIndex);
+        merge(array,startIndex,mid+1,endIndex);
     }
 
-    public static void mergeSort(int[] a, int low, int high) {
-        int mid = (low + high) / 2;
-        if (low < high) {
-            // 左边
-            mergeSort(a, low, mid);
-            // 右边
-            mergeSort(a, mid + 1, high);
-            // 左右归并
-            merge(a, low, mid, high);
-            System.out.println(Arrays.toString(a));
-        }
-
+    private static void merge(int[] array, int startIndex, int i, int endIndex) {
+        int length = endIndex-startIndex+1;
+        int[] tempArray = new int[length];
+        while (startIndex <)
     }
+
 
     @Test
-    public void testMerge(){
+    public void testMergeSort(){
         int a[] = {};
         mergeSort(a, 0, a.length - 1);
         System.out.println("排序结果：" + Arrays.toString(a));
