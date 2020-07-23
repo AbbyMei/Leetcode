b69ce51cac11ead60e492460bcb291397c8056a6
mei <abbymei1994@163.com>
2020-07-23 18:05:14 +0800

    1、spiralOrder

--- src/main/java/matrix/Offer29_spiralOrder.java
+++ src/main/java/matrix/Offer29_spiralOrder.java
@@ -1,16 +1,21 @@
 package matrix;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Offer29_spiralOrder {
     public int[] spiralOrder(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return new int[0];
+        }
         int tC = 0;
         int tR = 0;
-        int bC = matrix.length;
-        int bR = matrix[0].length;
+        int bC = matrix.length - 1;
+        int bR = matrix[0].length - 1;
         List<Integer> ret = new ArrayList<>();
-        while (tC < bC && tR < bR) {
+        while (tC <= bC && tR <= bR) {
             printEdge(matrix, tC++, tR++, bC--, bR--, ret);
         }
         return ret.stream().mapToInt(k -> k).toArray();
@@ -19,21 +24,31 @@ public class Offer29_spiralOrder {
     private List<Integer> printEdge(int[][] matrix, int tC, int tR, int bC, int bR, List<Integer> ret) {
         int curC = tC;
         int curR = tR;
+        if(curC == bC && curR == bR){
+            ret.add(matrix[curC][curR]);
+        }
         while(curR < bR){
-            ret.add(curC,curR);
+            ret.add(matrix[curC][curR]);
             curR++;
         }
         while(curC < bC){
-            ret.add(curC,curR);
+            ret.add(matrix[curC][curR]);
             curC++;
         }
         while(curR > tR){
-            ret.add(curC,curR);
+            ret.add(matrix[curC][curR]);
             curR--;
         }
         while(curC > tC){
-            ret.add(curC,curR);
+            ret.add(matrix[curC][curR]);
             curC--;
         }
+        return ret;
+    }
+
+    @Test
+    public void testSpiralOrder(){
+        int matrix[][] = {{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println(spiralOrder(matrix));
     }
 }
